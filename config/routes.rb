fb093e6997ed6620032929578321efafcedb7733
JudeYang <byoox@qq.com>
2017-03-13 11:41:46 +0800

    add_to

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-root 'product#index'
+root 'products#index'
   devise_for :users
   namespace :admin do
     resources :products
@@ -7,5 +7,9 @@ root 'product#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :products
+  resources :products do
+    member do
+      post :add_to_cart
+    end
+end
 end
