6fd51dffba0ca3d00bec3d8deed8e1370fa52fe9
JudeYang <byoox@qq.com>
2017-04-16 00:16:38 +0800

    add payment_method fuction

--- config/routes.rb
+++ config/routes.rb
@@ -21,4 +21,14 @@ end
   end
 resources :cart_items
 resources :orders
+
+namespace :account do
+  resources :orders do
+    member do
+      post :pay_with_alipay
+      post :pay_with_wechat
+    end
+  end
+ end
+
 end
