64c5d8e8ed4447cbaf9d3139d926145c84cbd62f
JudeYang <byoox@qq.com>
2017-04-16 01:46:17 +0800

    add assm_state to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,46 @@
 class Order < ApplicationRecord
-before_create :generate_token
-  belongs_to :user
+        before_create :generate_token
+
+        belongs_to :user
+        has_many :product_lists
+
+        validates :billing_name, presence: true
+        validates :billing_address, presence: true
+        validates :shipping_name, presence: true
+        validates :shipping_address, presence: true
+
+
+  include AASM
+
+    aasm do
+      state :order_placed, initial: true
+      state :paid
+      state :shipping
+      state :shipped
+      state :order_cancelled
+      state :good_returned
+
+
+      event :make_payment, after_commit: :pay! do
+        transitions from: :order_placed, to: :paid
+      end
+
+      event :ship do
+        transitions from: :paid,         to: :shipping
+      end
+
+      event :deliver do
+        transitions from: :shipping,     to: :shipped
+      end
+
+      event :return_good do
+        transitions from: :shipped,      to: :good_returned
+      end
+
+      event :cancel_order do
+        transitions from: [:order_placed, :paid], to: :order_cancelled
+      end
+    end
 
     def generate_token
         self.token = SecureRandom.uuid
@@ -14,10 +54,6 @@ before_create :generate_token
      self.update_columns(is_paid: true )
    end
 
-    validates :billing_name, presence: true
-    validates :billing_address, presence: true
-    validates :shipping_name, presence: true
-    validates :shipping_address, presence: true
-has_many :product_lists
+
 
 end
