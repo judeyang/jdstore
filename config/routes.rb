15b9d90926f4d6c55aea73a3381959e56fd44979
JudeYang <byoox@qq.com>
2017-05-26 19:08:31 +0800

    add app

--- config/routes.rb
+++ config/routes.rb
@@ -14,8 +14,6 @@ root 'products#index'
    end
   end
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   resources :products do
     member do
       post :add_to_cart
